5a3ffac61978ee019862060369cbea67e8e40eb3
son <son@lincolnix.net>
2014-06-25 09:23:26 +0000

    minor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,12 +20,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: "587",
-    domain: ENV["GMAIL_DOMAIN"],
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN_DEV"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: ENV["GMAIL_USERNAME_DEV"],
+    password: ENV["GMAIL_PASSWORD_DEV"]
   }
 
   # Print deprecation notices to the Rails logger.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   # ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => '587',
+    :port => 587,
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRIDE_PASSWORD'],
